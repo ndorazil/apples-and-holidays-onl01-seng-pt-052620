8d949b3f042913c834d06e7dfda3528215314b5c
Nathan Dorazil <nathand1915@gmail.com>
2020-06-10 00:05:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday_values|
     holiday_values.each do |holiday, values|
       holiday.collect do |supply|
-        supply.include?("BBQ")
+        if supply.include?("BBQ")
+          puts holiday
       end
     end
   end
